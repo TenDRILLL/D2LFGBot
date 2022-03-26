accce4de6e97d6295c03d872320cb863b012ca0a
TenDRILLL <32621403+TenDRILLL@users.noreply.github.com>
2022-03-26 23:34:53 +0200

    Implement loader for events on main.js

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const Enmap = require("enmap");
 const bot = new Client({intents: ["GUILDS"]});
 const {token} = require("./config.json");
 bot.db = new Enmap({name:"servers"});
-
+require("./automation/loader").createEvents(bot);
 bot.login(token).then(()=>console.log("Authentication successful.")).catch((e)=>console.log("Authentication failed; " + e));
