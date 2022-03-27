8745f165316f5de8a8e0166cf3341a56fc9d6572
TenDRILLL <32621403+TenDRILLL@users.noreply.github.com>
2022-03-27 02:01:30 +0200

    Create a default autoComplete method to override

--- automation/commandClass.js
+++ automation/commandClass.js
@@ -4,5 +4,6 @@ class Command {
     }
     get(){return this.slashObj;}
     exec(){return console.log(`${this.slashObj.name} was executed, but the method hasn't been overridden.`);}
+    autoComplete(interaction){interaction.respond([].map(x => ({name: x, value: x})))}
 }
 module.exports = Command;
