6bb192bbb28a5b2272f8a05bd187136664faefcd
TenDRILLL <32621403+TenDRILLL@users.noreply.github.com>
2022-03-27 02:41:47 +0200

    Create a default modalSubmit method to override

--- automation/commandClass.js
+++ automation/commandClass.js
@@ -3,7 +3,8 @@ class Command {
         this.slashObj = slashy;
     }
     get(){return this.slashObj;}
-    exec(){return console.log(`${this.slashObj.name} was executed, but the method hasn't been overridden.`);}
-    autoComplete(interaction){interaction.respond([].map(x => ({name: x, value: x})))}
+    exec(interaction){interaction.reply(`${this.slashObj.name} was executed, but the method hasn't been overridden.`);}
+    autoComplete(interaction){interaction.respond([].map(x => ({name: x, value: x})));}
+    modalSubmit(interaction){interaction.reply(`${this.slashObj.name} got a modalsubmit, but the method hasn't been overridden.`);}
 }
 module.exports = Command;
